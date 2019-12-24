3220aeb54ba44f98f053e051a31e4be4f79f494e
Curt Kaler <curt.kaler@gmail.com>
2019-12-23 16:23:16 -0800

    Update dependencies; fix webpack config

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -1,32 +1,27 @@
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const TerserPlugin = require("terser-webpack-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const merge = require("webpack-merge");
 const path = require("path");
 
 //==========================================================================================
 
 const LIBRARY_NAME = "testing";
-const AUTURGE_ROOT = path.resolve(__dirname, "../../..");
-const PACKAGE_ROOT = path.resolve(__dirname, "../");
-const webpackBase = path.resolve(AUTURGE_ROOT, "./conf/webpack.config.base.js");
+const PROJECT_ROOT = path.resolve(__dirname, "../");
 
 // Specify where webpack should start the bundling process
-const entryPath = path.resolve(PACKAGE_ROOT, "./src/index.ts");
+const entryPath = path.resolve(PROJECT_ROOT, "./src/index.ts");
 
 // Specify where webpack should place the built bundles
-const bundlePath = path.resolve(
-    AUTURGE_ROOT,
-    `./built/${LIBRARY_NAME}/bundles`
-);
+const bundlePath = path.resolve(PROJECT_ROOT, `./built/bundles`);
 
-const testsPath = path.resolve(PACKAGE_ROOT, "./tests");
+const testsPath = path.resolve(PROJECT_ROOT, "./tests");
 
 // specify the tsConfig project file to use
-const tsConfig = path.resolve(PACKAGE_ROOT, "./src/tsconfig.json");
+const tsConfig = path.resolve(PROJECT_ROOT, "./src/tsconfig.json");
 
 //==========================================================================================
 
 // identify paths
-// const PROJECT_ROOT = path.resolve(__dirname, '../');
 var PATHS = {
     entryPoint: entryPath,
     bundles: bundlePath,
@@ -38,29 +33,48 @@ let entry = {};
 entry[`${LIBRARY_NAME}.umd`] = [PATHS.entryPoint];
 entry[`${LIBRARY_NAME}.umd.min`] = [PATHS.entryPoint];
 
-// import the base webpack config
-const baseInfo = require(webpackBase);
+let config = {
+    // Activate source maps for the bundles in order to preserve the original
+    // source when the user debugs the application
+    devtool: "source-map",
 
-let config = merge(baseInfo, {
     // These are the entry point of our library. We tell webpack to use
     // the name we assign later, when creating the bundle. We also use
     // the name to filter the second entry point for applying code
     // minification via UglifyJS
     entry: entry,
 
-    // The output defines how and where we want the bundles. The special
-    // value `[name]` in `filename` tell Webpack to use the name we defined
-    // above.
-    // We target a UMD and name it scatter. When including the bundle in the
-    // browser
-    // it will be accessible at `window.scatter`
-    output: {
-        path: PATHS.bundles,
-        filename: "[name].js",
-        libraryTarget: "umd",
-        library: LIBRARY_NAME,
-        umdNamedDefine: true
+    // Tell webpack not to bundle the following things, if it sees them
+    externals: {
+        electron: "electron",
+        child_process: "child_process",
+        crypto: "crypto",
+        events: "events",
+        fs: "fs",
+        http: "http",
+        https: "https",
+        assert: "assert",
+        dns: "dns",
+        minimist: "minimist",
+        net: "net",
+        os: "os",
+        path: "path",
+        querystring: "querystring",
+        readline: "readline",
+        repl: "repl",
+        stream: "stream",
+        string_decoder: "string_decoder",
+        url: "url",
+        util: "util",
+        zlib: "zlib",
+        lodash: {
+            commonjs: "lodash",
+            amd: "lodash",
+            root: "_" // indicates global variable
+        }
     },
+
+    mode: "production",
 
     module: {
         // Webpack doesn't understand TypeScript files and a loader is needed.
@@ -83,10 +97,57 @@ let config = merge(baseInfo, {
         ]
     },
 
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // Apply minification only on the second bundle by
+            // using a RegEx on the name, which must end with `.min.js`
+            // NB: Remember to activate sourceMaps in TerserPlugin
+            // since they are disabled by default!
+            new TerserPlugin({
+                include: /\.min\.js$/,
+                parallel: true,
+                sourceMap: true,
+                terserOptions: {
+                    ecma: 8,
+                    warnings: false,
+                    output: { comments: false, beautify: false },
+                    toplevel: false,
+                    nameCache: null,
+                    ie8: false,
+                    keep_classnames: undefined,
+                    keep_fnames: false,
+                    safari10: false
+                }
+            })
+        ],
+        noEmitOnErrors: true
+    },
+
+    // The output defines how and where we want the bundles. The special
+    // value `[name]` in `filename` tell Webpack to use the name we defined
+    // above.
+    // We target a UMD and name it after the library. When including the bundle in the
+    // browser
+    // it will be accessible at `window.<LIBRARY_NAME>`
+    output: {
+        path: PATHS.bundles,
+        filename: "[name].js",
+        libraryTarget: "umd",
+        library: LIBRARY_NAME,
+        umdNamedDefine: true
+    },
+
+    plugins: [new CheckerPlugin()],
+
     resolve: {
+        // Add resolve for `tsx` and `ts` files, otherwise Webpack would
+        // only look for common JavaScript file extension (.js)
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+
         plugins: [new TsconfigPathsPlugin({ configFile: PATHS.tsConfig })]
     }
-});
+};
 
 function describePackJob() {
     const showConfig = false;
@@ -105,8 +166,7 @@ function describePackJob() {
     );
     console.log(" ");
     console.log(`        NODE_ENV: [${NODE_ENV}]`);
-    console.log(`    PROJECT_ROOT: [${AUTURGE_ROOT}]`);
-    console.log(`    PACKAGE_ROOT: [${PACKAGE_ROOT}]`);
+    console.log(`    PROJECT_ROOT: [${PROJECT_ROOT}]`);
     console.log(`       entryPath: [${PATHS.entryPoint}]`);
     console.log(`        tsConfig: [${PATHS.tsConfig}]`);
     console.log(`    BUNDLES PATH: [${PATHS.bundles}]`);
